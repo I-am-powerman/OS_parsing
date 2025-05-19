74b644228e2a73acb062fca3ebdc41151a87761c
I-am-powerman <seredkin.dmitrii.powerman@gmail.com>
2025-05-19 22:48:44 +0800

    2025-05-19.2

---
+++ main.py
@@ -0,0 +1,5 @@
+import parsing_to_txt as p
+
+test = p.parsing_to_txt("progekt")
+
+print(test.txt_max_path())
--- parsing_to_txt.py
+++ parsing_to_txt.py
@@ -1,20 +1,31 @@
 import os
 
-
-b = os.path.isdir(original_path)
-c = os.listdir(original_path)
-
 class parsing_to_txt():
-    original_path = "progekt"
+    max_path = {}
+    cickle_iter = 0
 
+    def __init__(self, original_path: str):
+        self.original_path :str = original_path
 
+    def read_file(self, path: str):
+        if os.path.isdir(path):
+            list_directory = os.listdir(path)
+            for i in range(len(list_directory)):
+                print(self.cickle_iter)
+                print(ord(path))
+                self.read_file(path + "/" + list_directory[i])
+                self.cickle_iter += 1
+        else:
+            self.max_path[path] = path
+        
     
-""" создать функцию которая принимает директорию
-проверяет является ли она директоией, если да, 
-то проваливается ниже и повторяет процесс, если нет то
-записывает путь до файла, что бы сравнить его в дальнейшем
-(записывает весь путь, так как str тоже можно сравнивать)
-"""
+    def txt_max_path(self):
+       self.read_file(self.original_path)
+       max_keys = max(self.max_path.keys())
+       print(self.max_path)
+       return self.max_path[max_keys]
+    
+    def write_path(self):
+        pass
 
-
-print(c)
+"""нужно убрать повторения"""
