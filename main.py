8cfd57d4c18fa112c1947e8535d2ebc5a9a8a006
I-am-powerman <seredkin.dmitrii.powerman@gmail.com>
2025-05-20 10:39:32 +0800

    2025-05-20

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import parsing_to_txt as p
 
 test = p.parsing_to_txt("progekt")
 
-print(test.txt_max_path())
+print(test.docx_max_path())
+print(test.docx_min_path())
--- parsing_to_txt.py
+++ parsing_to_txt.py
@@ -1,31 +1,37 @@
 import os
 
 class parsing_to_txt():
-    max_path = {}
-    cickle_iter = 0
+    path_docx = {}
 
     def __init__(self, original_path: str):
         self.original_path :str = original_path
+    
+    def docx_max_path(self):
+       self._read_file(self.original_path)
+       max_keys = max(self.path_docx.keys())
+       file_max_path = self.path_docx[max_keys]
 
-    def read_file(self, path: str):
+       return file_max_path
+    
+    def docx_min_path(self):
+       self._read_file(self.original_path)
+       min_keys = min(self.path_docx.keys())
+       file_min_path = self.path_docx[min_keys]
+
+       return file_min_path
+    
+    def _read_file(self, path: str):
         if os.path.isdir(path):
             list_directory = os.listdir(path)
             for i in range(len(list_directory)):
-                print(self.cickle_iter)
-                print(ord(path))
-                self.read_file(path + "/" + list_directory[i])
-                self.cickle_iter += 1
+                self._read_file(path + "/" + list_directory[i])
         else:
-            self.max_path[path] = path
-        
+            iter = len(path.split("/"))
+            docx = self._write_docx(path)
+            self.path_docx[iter] = docx
     
-    def txt_max_path(self):
-       self.read_file(self.original_path)
-       max_keys = max(self.max_path.keys())
-       print(self.max_path)
-       return self.max_path[max_keys]
-    
-    def write_path(self):
-        pass
+    def _write_docx(self, docx: str) -> str:
+        name_docx = docx.split("/")
+        name_docx = name_docx[-1]
 
-"""нужно убрать повторения"""
+        return name_docx
